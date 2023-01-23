0e26a9a62c2f8624ffac67a0afbaebf4c60ad8d4
dodgerogers <dodgerogers@hotmail.com>
2023-01-22 19:28:36 -0500

    Rubocop fixes

--- lib/nucleus/basic_object.rb
+++ lib/nucleus/basic_object.rb
@@ -2,7 +2,7 @@ module Nucleus
   class BasicObject
     def initialize(attrs={})
       attrs.each do |key, value|
-        self.instance_variable_set("@#{key}", value)
+        instance_variable_set("@#{key}", value)
       end
     end
   end
