fe81ef1fc4d573ee4f051f61a315d26d69be9aab
Andrew Rogers <andrew@gobolt.com>
2024-02-09 14:15:14 +0000

    Configurable workflow process state persistance

--- test/support/test_repositories.rb
+++ test/support/test_repositories.rb
@@ -6,4 +6,12 @@ class TestRepository < NucleusCore::Repository
       OpenStruct.new(id: id, ref: SecureRandom.hex)
     end
   end
+
+  def self.persist_process(_process, _attrs={})
+    nil
+  end
+
+  def self.failing_persist_process(_process, _attrs={})
+    false
+  end
 end
