1f01b21451bbc38b8b80a96139eb85ceb44a9a86
Andrew Rogers <andrew@gobolt.com>
2024-09-21 15:19:32 -0400

    Extract view response format attributes to configuration

--- lib/nucleus_core/view_response_implementation.rb
+++ lib/nucleus_core/view_response_implementation.rb
@@ -1,18 +1,8 @@
 module NucleusCore
   class ViewResponseImplementation < NucleusCore::SimpleObject
-    FORMAT_ATTRIBUTES = {
-      csv: { disposition: "attachment", type: "text/csv; charset=UTF-8;", filename: "response.csv" },
-      pdf: { disposition: "inline", type: "application/pdf", filename: "response.pdf" },
-      json: { type: "application/json", format: :json },
-      xml: { type: "application/xml", format: :xml },
-      html: { type: "text/html", format: :html },
-      text: { type: "text/plain", format: :text },
-      nothing: { content: nil, type: "text/html; charset=utf-8", format: :nothing }
-    }.freeze
-
     def initialize(format, attrs={})
       default_attrs = { content: nil, headers: nil, status: nil, location: nil, format: nil }
-      format_attrs = FORMAT_ATTRIBUTES[format]
+      format_attrs = NucleusCore.configuration.response_formats[format] || {}
       status = status_code(attrs[:status])
 
       super(
