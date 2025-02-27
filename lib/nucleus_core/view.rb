e3a87b0b458bd175d0a9e3ba8a51645e74d1091e
Andrew Rogers <andrew@gobolt.com>
2025-02-27 06:34:08 -0500

    Helper method for view repsonse initialization, and collapse of view response implementation class

--- lib/nucleus_core/view.rb
+++ lib/nucleus_core/view.rb
@@ -1,12 +1,49 @@
 require "nucleus_core/entity"
-require "nucleus_core/view_response_implementation"
 
 module NucleusCore
   class View < NucleusCore::Entity
-    class Response < NucleusCore::ViewResponseImplementation; end
+    class Response < NucleusCore::Entity
+      def initialize(format, attrs={})
+        status = status_code(attrs[:status])
 
+        super(attrs.merge!(format: format, status: status))
+      end
+
+      private
+
+      def status_code(status=nil)
+        status = Utils.status_code(status)
+
+        status.zero? ? 200 : status
+      end
+    end
+
+    # Equivalent to build_response(request_format: :json, content: {...}, status: :ok)
     def json
-      NucleusCore::View::Response.new(:json, content: to_h, status: :ok)
+      build_response(content: to_h, status: :ok)
+    end
+
+    private
+
+    # Creates a new `NucleusCore::View::Response` object with the given format and attributes.
+    #
+    # @param request_format [Symbol] The format of the response (e.g., `:html`, `:json`).
+    #   Defaults to the name of the calling method if not provided.
+    # @param attrs [Hash] Additional attributes for the response.
+    #
+    # @return [NucleusCore::View::Response] A new response object.
+    #
+    # @example Default format (inferred from caller)
+    #   def show
+    #     build_response(title: "Example")
+    #     # Equivalent to build_response(request_format: :show, title: "Example")
+    #   end
+    #
+    # @example Explicit format
+    #   build_response(request_format: :json, data: { foo: "bar" })
+    #
+    def build_response(request_format: caller_locations(1, 1).first.label.to_sym, **attrs)
+      NucleusCore::View::Response.new(request_format, attrs || {})
     end
   end
 end
